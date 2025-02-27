aad6c4a04d41947e3f52f1d89b0937cdc0c23698
Justin Paulson <justinapaulson@gmail.com>
2025-02-27 16:51:54 -0600

    Fix turbo bug

--- app/models/message.rb
+++ app/models/message.rb
@@ -11,9 +11,5 @@ class Message < ApplicationRecord
 
   scope :for_bot, ->(bot) { where(bot: bot) }
 
-  # broadcasts_to ->(message) { [message.bot, "messages"] }, action: :prepend
-
-  # Ideally we would be using the above line instead of the two below, but there is a bug in rails/turbo
-  after_create_commit -> { broadcast_prepend_to [bot, "messages"]}
-  after_update_commit -> { broadcast_prepend_to [bot, "messages"]}
+  broadcasts_to ->(message) { [message.bot, "messages"] }, inserts_by: :prepend
 end
